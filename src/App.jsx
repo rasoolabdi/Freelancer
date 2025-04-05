02e1f4996edfbe3c1c86378166861b4682f8db69
rasoolabdi <M0rd00r@yahoo.com>
2025-04-05 06:48:34 -0700

    added tailwindcss and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
-    <div>freelancer</div>
+    <div className='container xl: max-w-screen-xl bg-primary-200'>
+      <span className='bg-primary-300 text-secondary-900 bg-opacity-100 font-bold text-3xl'>freelancer</span>
+    </div>
   )
 }
 export default App;
