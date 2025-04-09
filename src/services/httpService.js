920e3e211db3f2f301536a2f32c69259ff7ce956
rasoolabdi <M0rd00r@yahoo.com>
2025-04-09 01:35:15 -0700

    added Handling create new refreshToken and accessToken

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,38 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:5000/api"; 
+
 const app = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: BASE_URL,
     withCredentials: true
 });
+
+
+app.interceptors.request.use(
+    (res) => res,
+    (error) => Promise.reject(error)
+);
+
+app.interceptors.response.use(
+    (res) => res,
+    async (error) => {
+        const originalConfig = error.config;
+        if(error.response.status === 401 && !originalConfig._retry) {
+            originalConfig._retry = true;
+            try {
+                const { data } = await axios.get(`${BASE_URL}/user/refresh-token` , {withCredentials: true});
+                if(data) {
+                    return app(originalConfig);
+                }
+            }
+            catch(error) {
+                return Promise.reject(error)
+            }
+        }
+        return Promise.reject(error);
+    } 
+)
+
 
 
 const http = {
