a07584150b0c64f448da395fcd35dd28e91e363d
rasoolabdi <M0rd00r@yahoo.com>
2025-04-11 23:59:48 -0700

    added dynamic table

--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -1,8 +1,7 @@
 import Empty from "../../ui/Empty";
 import Loading from "../../ui/Loading";
-import tolocalDateShort from "../../utils/toLocalDateShort";
-import toPersianNumbersWithComma from "../../utils/toPersianNumbers";
-import truncateText from "../../utils/truncateText";
+import Table from "../../ui/Table";
+import ProjectRow from "./ProjectRow";
 import useOwnerProjects from "./useOwnerProjects";
 
 
@@ -14,50 +13,24 @@ function ProjectTable() {
     if(!projects.length) return <Empty resourceName="پروژه ایی" />;
 
     return (
-        <div className="bg-secondary-0 overflow-x-auto">
-            <table>
-                <thead>
-                    <tr className="title-row">
-                        <th>#</th>
-                        <th>عنوان پروژه</th>
-                        <th>دسته بندی</th>
-                        <th>بودجه</th>
-                        <th>ددلاین</th>
-                        <th>تگ ها</th>
-                        <th>فریلنسر</th>
-                        <th>وضعیت</th>
-                        <th>عملیات</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {projects.map((project , index) => (
-                        <tr key={project._id}>
-                            <td>{index + 1}</td>
-                            <td>{truncateText(project.title , 30)}</td>
-                            <td>{project.category.title}</td>
-                            <td>{toPersianNumbersWithComma(project.budget)}</td>
-                            <td>{tolocalDateShort(project.deadline)}</td>
-                            <td>
-                                <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
-                                    {project.tags.map((tag) => (
-                                        <span className="badge badge--secondary" key={tag}>{tag}</span>
-                                    ))}
-                                </div>
-                            </td>
-                            <td>{project.freelancer?.name || "-"}</td>
-                            <td>
-                                {project.status === "OPEN" ? (
-                                    <span className="badge badge--success">باز</span>
-                                ) : (
-                                    <span className="badge badge--danger">بسته</span>
-                                )}
-                            </td>   
-                            <td>...</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <Table>
+            <Table.Header>
+                <th>#</th>
+                <th>عنوان پروژه</th>
+                <th>دسته بندی</th>
+                <th>بودجه</th>
+                <th>ددلاین</th>
+                <th>تگ ها</th>
+                <th>فریلنسر</th>
+                <th>وضعیت</th>
+                <th>عملیات</th>
+            </Table.Header>
+            <Table.Body>
+            {projects.map((project , index) => (
+                <ProjectRow project={project} index={index} key={project._id} />
+            ))}
+            </Table.Body>
+        </Table>
     )
 };
 export default ProjectTable;    
