e172250e82c6637e8204da94259e19c74636f0f2
rasoolabdi <M0rd00r@yahoo.com>
2025-04-13 22:39:34 -0700

    added hook outsideClick

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,9 +1,12 @@
 import { HiOutlineXCircle } from "react-icons/hi2";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function Modal({children , open , onClose , title}) {
+    const ref = useOutsideClick(onClose);
+
     return (open && 
         <div className="backdrop-blur-sm fixed top-0 left-0 w-full h-screen bg-secondary-800 bg-opacity-30 z-50">
-            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-secondary-0
+            <div ref={ref} className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-secondary-0
                         p-4 shadow-lg transition-all duration-500 ease-out w-[calc(100vw - 2rem)] md:max-w-lg max-h-[calc(100vh-2rem)]
                         overflow-y-auto
             ">
