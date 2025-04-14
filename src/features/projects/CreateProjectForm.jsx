40707810fba4b318227feed924b690f5e0a33b0c
rasoolabdi <M0rd00r@yahoo.com>
2025-04-14 00:58:10 -0700

    added form create new project by owner

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -1,11 +1,71 @@
+import { useForm } from "react-hook-form";
 import TextField from "../../ui/TextField";
 
 
 function CreateProjectForm() {
+    const {register , formState: {errors} , handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log("dd" , data)
+    }
 
     return (
-        <form>
-            
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+            <TextField 
+                label="عنوان پروژه"
+                name="title"
+                register={register}
+                required
+                validationSchema={{
+                    required: "لطفا عنوان پروژه را وارد نمایید",
+                    minLength: {
+                        value: 5,
+                        message: "طول عنوان پروژه کمتر از پنج کاراکتر نباشد"
+                    },
+                    maxLength: {
+                        value: 20,
+                        message: "طول عنوان پروژه بیشتر از بیست کاراکتر نباشد"
+                    }
+                }}
+                errors={errors}
+            />
+            <TextField 
+                label=" توضیحات پروژه"
+                name="description"
+                register={register}
+                required
+                validationSchema={{
+                    required: "لطفا توضیحات پروژه را وارد نمایید",
+                    minLength: {
+                        value: 10,
+                        message: " توضیحات پروژه کمتر از ده کاراکتر نباشد"
+                    },
+                    maxLength: {
+                        value: 50,
+                        message: "توضیحات پروژه بیشتر از پنجاه کاراکتر نباشد"
+                    }
+                }}
+                errors={errors}
+            />
+            <TextField 
+                label="بودجه پیشنهادی کارفرما"
+                name="budget"
+                register={register}
+                required
+                validationSchema={{
+                    required: "لطفا بودجه پیشنهادی خود را وارد نمایید",
+                    minLength: {
+                        value: 5,
+                        message: "رقم بودجه کمتر از 5 پنج عدد نباشد"
+                    },
+                    maxLength: {
+                        value: 15,
+                        message: "رقم بودجه بیشتر از پانزده عدد نباشد"
+                    }
+                }}
+                errors={errors}
+            />
+            <button className="btn btn--primary w-full" type="submit">تایید</button>
         </form>
     )
 };
--- src/ui/TextField.jsx
+++ src/ui/TextField.jsx
@@ -1,17 +1,23 @@
-function TextField({ name , value , placeholder , onChange , label , type="text" }) {
+function TextField({ name , register , placeholder = ""  , label , type="text" , required , validationSchema , errors}) {
+    console.log({... register(name)})
     return (
         <div>
-            <label htmlFor={name} className="mb-2">{label}</label>
+            <label htmlFor={name} className="mb-2 block text-secondary-700">
+                {label}
+                {required && <span className="text-error">*</span>}
+            </label>
             <input 
                 type={type} 
                 id={name}
                 name={name}
-                value={value}
-                onChange={onChange}
+                { ... register(name , validationSchema) }
+                // value={value}
+                // onChange={onChange}
                 placeholder={placeholder}
                 className="textField__input"
                 autoComplete="off"
             />
+            {errors && errors[name] && <span className="text-error block text-sm mt-2">{errors[name]?.message}</span>}
         </div>
     )
 };
