54fec40839e0d31e083d750c45275fd8e9146f68
rasoolabdi <M0rd00r@yahoo.com>
2025-04-26 05:04:13 -0700

    added api query string for filterings

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ import FreelancerDashboard from './pages/FreelancerDashboard';
 import Proposals from './pages/Proposals';
 import SubmittedProjects from './pages/SubmittedProjects';
 import FreelancerLayout from './features/freelancer/FreelancerLayout';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 
 function App() {
   const queryClient = new QueryClient();
@@ -22,29 +24,31 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
-        <Toaster />
-        <div>
-          <Routes>
-            <Route path='/auth' element={<Auth />} />
-            <Route path='/complete-profile' element={<CompleteProfile />} />
-            <Route path="/owner" element={<OwnerLayout />}>
-                <Route index element={<Navigate to="dashboard" replace />} />
-                <Route path="dashboard" element={<OwnerDashboard />} />
-                <Route path='projects' element={<Projects />} />
-                <Route path='projects/:id' element={<Project />} />
-            </Route>
-            <Route path='/freelancer' element={<FreelancerLayout />}>
-                <Route index element={<Navigate to="dashboard" replace />} />
-                <Route path='dashboard' element={<FreelancerDashboard />} />
-                <Route path='proposals' element={<Proposals />} />
-                <Route path='projects' element={<SubmittedProjects />} />
-            </Route>
-            <Route path="/" element={<Home />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
+        {/* <ReactQueryDevtools initialIsOpen={false}>   */}
+          <Toaster />
+          
+            <Routes>
+              <Route path='/auth' element={<Auth />} />
+              <Route path='/complete-profile' element={<CompleteProfile />} />
+              <Route path="/owner" element={<OwnerLayout />}>
+                  <Route index element={<Navigate to="dashboard" replace />} />
+                  <Route path="dashboard" element={<OwnerDashboard />} />
+                  <Route path='projects' element={<Projects />} />
+                  <Route path='projects/:id' element={<Project />} />
+              </Route>
+              <Route path='/freelancer' element={<FreelancerLayout />}>
+                  <Route index element={<Navigate to="dashboard" replace />} />
+                  <Route path='dashboard' element={<FreelancerDashboard />} />
+                  <Route path='proposals' element={<Proposals />} />
+                  <Route path='projects' element={<SubmittedProjects />} />
+              </Route>
+              <Route path="/" element={<Home />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          
+        {/* </ReactQueryDevtools> */}
       </QueryClientProvider>
     </DarkModeProvider>
-  )
+  )s
 }
 export default App;
--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProjectsApi } from "../services/projectService";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 function useProjects() {
+    const { search } = useLocation();
+    const queryObject = queryString.parse(search);
+    
     const {isLoading , data} = useQuery({
-        queryKey: ["projects"],
-        queryFn: getProjectsApi,
+        queryKey: ["projects" , queryObject],
+        queryFn: () => getProjectsApi(search),
         retry: false,
         refetchOnReconnect: true,
         refetchOnWindowFocus: true
